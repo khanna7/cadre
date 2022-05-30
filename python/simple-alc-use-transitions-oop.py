f4de2bc932b69571c6704d238c4a46ded3d15397
Aditya Khanna <khanna7.work@gmail.com>
2022-05-30 12:07:19 -0400

    compute and print avg age and median alc use

--- python/simple-alc-use-transitions-oop.py
+++ python/simple-alc-use-transitions-oop.py
@@ -1,4 +1,5 @@
 from numpy import random
+import statistics
 
 class Person():
     def __init__(self, name=None, age=None, alc_use_status=None):
@@ -15,15 +16,26 @@ class Person():
         print("This object's name is {}.".format(self.name))
 
 def main():
+    age_sum = 0
+    alc_use_status = {}
+
     my_persons = {}
     for i in range(11):
         my_persons[i] = Person()
         my_persons[i].name = i
+        age_sum = my_persons[i].age + age_sum
+        alc_use_status[i] = my_persons[i].alc_use_status
 
         print(my_persons[i].name)
         print(my_persons[i].age)
         print(my_persons[i].alc_use_status, "\n")
-        print(len(my_persons))
+    
+    
+    print((alc_use_status.values()), "\n")
+    print("Total number of persons is: " + str(len(my_persons)))
+    print("Their average age is: " + str(age_sum/len(my_persons)))
+    print("Their median level of alcohol use is " + 
+    str(statistics.median(alc_use_status.values())))
 
     
 
