645b76654ad9d5accfb4dd7781451c171a210bec
Aditya Khanna <khanna7.work@gmail.com>
2022-05-30 14:24:13 -0400

    correct use of self in initialization function

--- python/simple-alc-use-transitions-oop.py
+++ python/simple-alc-use-transitions-oop.py
@@ -12,7 +12,7 @@ class Person():
         self.age = age
         self.alc_use_status = alc_use_status
 
-    def initialize_population(self, n=11):
+    def initialize_population(n:int=10):
         my_persons = {}
         age_sum = 0
         alc_use_status = {}
@@ -30,14 +30,21 @@ class Person():
         
         alc_use_status_vals = alc_use_status.values()
 
+        print("Number of agents is: " + 
+            str(len(my_persons)))
         print("Mean agent age is: " + 
             str(age_sum/len(my_persons)))
         print("Median level of alcohol use is " + 
             str(statistics.median(alc_use_status_vals)))
 
+    # add alcohol transition function here
+    def transition_alc_use(self, max=1):
+        print(random.random(max))
+
+
 def main():
-    n = 10000
-    Person.initialize_population(n)
+    Person.initialize_population(1001)
+    #Person.transition_alc_use(1)
   
 if __name__ == "__main__":
     main()
