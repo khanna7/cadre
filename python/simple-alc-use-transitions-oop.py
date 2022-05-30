c4142c6737ffcb7ee5acf4b4a8de69ee52dd6788
Aditya Khanna <khanna7.work@gmail.com>
2022-05-30 13:23:21 -0400

    clean up main function

--- python/simple-alc-use-transitions-oop.py
+++ python/simple-alc-use-transitions-oop.py
@@ -12,36 +12,34 @@ class Person():
         self.age = age
         self.alc_use_status = alc_use_status
 
-    def print_person_characteristics(self):
-        print("This object's name is {}.".format(self.name))
+    def initialize_population(self, n=11):
+        my_persons = {}
+        age_sum = 0
+        alc_use_status = {}
+        alc_use_status_vals = {}
+        
+        for i in range(n):
+            my_persons[i] = Person()
+            my_persons[i].name = i
+            age_sum = my_persons[i].age + age_sum
+            alc_use_status[i] = my_persons[i].alc_use_status
+
+            print(my_persons[i].name)
+            print(my_persons[i].age)
+            print(my_persons[i].alc_use_status, "\n")
+        
+        alc_use_status_vals = alc_use_status.values()
+
+        print("Mean agent age is: " + 
+            str(age_sum/len(my_persons)))
+        print("Median level of alcohol use is " + 
+            str(statistics.median(alc_use_status_vals)))
 
 def main():
-    age_sum = 0
-    alc_use_status = {}
-
-    my_persons = {}
-    for i in range(11):
-        my_persons[i] = Person()
-        my_persons[i].name = i
-        age_sum = my_persons[i].age + age_sum
-        alc_use_status[i] = my_persons[i].alc_use_status
-
-        print(my_persons[i].name)
-        print(my_persons[i].age)
-        print(my_persons[i].alc_use_status, "\n")
-    
-    
-    print((alc_use_status.values()), "\n")
-    print("Total number of persons is: " + str(len(my_persons)))
-    print("Their average age is: " + str(age_sum/len(my_persons)))
-    print("Their median level of alcohol use is " + 
-    str(statistics.median(alc_use_status.values())))
-
-    
-
-
-
-
+    n = 10000
+    Person.initialize_population(n)
+  
 if __name__ == "__main__":
     main()
+    
         
