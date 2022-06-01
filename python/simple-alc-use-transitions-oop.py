0f7036c9898d89d6b7d0a660ed0036a171f7901c
Aditya Khanna <khanna7.work@gmail.com>
2022-06-01 14:13:45 -0400

    revise program with daniel

--- python/simple-alc-use-transitions-oop.py
+++ python/simple-alc-use-transitions-oop.py
@@ -12,40 +12,8 @@ class Person():
         self.age = age
         self.alc_use_status = alc_use_status
 
-    def initialize_population(n:int=10):
-        # QT: didn't use self here because then I couldn't override the default param val in main
-        my_persons = {}
-        age_sum = 0
-        alc_use_status = {}
-        alc_use_status_vals = {}
-        
-        # initialize agents and attributes
-        for i in range(n):
-            my_persons[i] = Person()
-            my_persons[i].name = i
-            age_sum = my_persons[i].age + age_sum
-            alc_use_status[i] = my_persons[i].alc_use_status
+    def transition_alc_use(self):
 
-            print(my_persons[i].name)
-            print(my_persons[i].age)
-            print(my_persons[i].alc_use_status, "\n")
-        
-        alc_use_status_vals = alc_use_status.values()
-
-        print("Number of agents is: " + 
-            str(len(my_persons)))
-        print("Mean agent age is: " + 
-            str(age_sum/len(my_persons)))
-        print("Median level of alcohol use is " + 
-            str(statistics.median(alc_use_status_vals)))
-        print("Max level of alcohol use is " + 
-            str(max(alc_use_status_vals)))
-        print("Min level of alcohol use is " + 
-            str(min(alc_use_status_vals)))
-
-        # transition agents from alc use states
-        # QT: Can this be its own method? If so, how do I get it to access the objects it needs from above methods? 
-        # transition probabilities
         # level up
         trans_prob_0_1 = 0/100 
         trans_prob_1_2 = 1/100
@@ -56,50 +24,82 @@ class Person():
         trans_prob_3_2 = 0.5/100
 
         changes = 0
+        prob = random.uniform(0, 1)
+        if self.alc_use_status == 0:
+            if (prob <= trans_prob_0_1):
+                self.alc_use_status += 1
+                changes+=1
+                #print("change!")
 
-        for i in range(len(my_persons)):
-            prob = random.uniform(0, 1)
+        elif self.alc_use_status == 1:
+            if (prob <= trans_prob_1_2):
+                self.alc_use_status += 1
+                changes+=1
+                #print("change!")
+            elif (prob > 1-trans_prob_1_0):
+                self.alc_use_status -= 1
+                changes += 1
+                #print("change!")
+        
+        elif self.alc_use_status == 2:
+            if (prob <= trans_prob_2_3):
+                self.alc_use_status += 1
+                changes += 1
+                #print("change!")
+            elif (prob > 1-trans_prob_2_1):
+                self.alc_use_status -= 1
+                changes += 1
+                #print("change!")
 
-            if my_persons[i].alc_use_status == 0:
-                if (prob <= trans_prob_0_1):
-                    my_persons[i].alc_use_status += 1
-                    changes+=1
-                    print("change!")
+        elif self.alc_use_status == 3:
+            if (prob > 1-trans_prob_3_2):
+                self.alc_use_status -= 1
+                changes += 1
+                #print("change!")
 
-            elif my_persons[i].alc_use_status == 1:
-                if (prob <= trans_prob_1_2):
-                    my_persons[i].alc_use_status += 1
-                    changes+=1
-                    print("change!")
-                elif (prob > 1-trans_prob_1_0):
-                    my_persons[i].alc_use_status -= 1
-                    changes += 1
-                    print("change!")
-            
-            elif my_persons[i].alc_use_status == 2:
-                if (prob <= trans_prob_2_3):
-                    my_persons[i].alc_use_status += 1
-                    changes += 1
-                    print("change!")
-                elif (prob > 1-trans_prob_2_1):
-                    my_persons[i].alc_use_status -= 1
-                    changes += 1
-                    print("change!")
+def initialize_population(n):
+    # QT: didn't use self here because then I couldn't override the default param val in main
+    my_persons = [] #use array instead
+    age_sum = 0
+    alc_use_status = []
+    alc_use_status_vals = []
+    
+    # initialize agents and attributes
+    for i in range(n):
+        my_persons.append(Person(i))
+        age_sum = my_persons[i].age + age_sum
+        alc_use_status.append(my_persons[i].alc_use_status)  
 
-            elif my_persons[i].alc_use_status == 3:
-                if (prob > 1-trans_prob_3_2):
-                    my_persons[i].alc_use_status -= 1
-                    changes += 1
-                    print("change!")
+        print(my_persons[i].name)
+        print(my_persons[i].age)
+        print(my_persons[i].alc_use_status, "\n")
 
-        print("Number of transitions is " + str(changes))
+    print("Number of agents is: " + 
+        str(len(my_persons)))
+    print("Mean agent age is: " + 
+        str(age_sum/len(my_persons)))
+    print("Max level of alcohol use is " + 
+        str(max(alc_use_status)))
+    print("Min level of alcohol use is " + 
+        str(min(alc_use_status)))
 
-    # add alcohol transition function here
-    def transition_alc_use():
-        pass
+    return my_persons
+
     
 def main():
-    Person.initialize_population(10001) #QT: See above for overriding default params
+    persons = initialize_population(10001) 
+
+    MAXTIME=1000
+
+    
+
+    for time in range(MAXTIME):
+        print("Timestep = " + str(time))
+        
+        for person in persons:
+            person.transition_alc_use()
+
+    #QT: See above for overriding default params
     #Person.transition_alc_use()
 
     #QT: Where would a time loop go?
