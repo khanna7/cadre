8a7d32b6ef72510b51e03dd54a4b03ede8f6a173
Aditya Khanna <khanna7.work@gmail.com>
2022-06-18 15:15:49 -0400

    expecting test to pass, but it is failing

--- python/src/test_person.py
+++ python/src/test_person.py
@@ -5,33 +5,22 @@ import cadre_model
 
 class TestSum(unittest.TestCase):
 
-    person = cadre_person.Person()
+    def test_age_assignment(self):
+        ages = []
+        min_age = 18
+        max_age = 65
 
-    def test_sum(self):
-         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-         self.assertTrue(sum([1, 2, 3]) == 6, "Should be 6")
-
-    # def test_age_assignment(self):
-    #     ages = []
-    #     min_age = 40
-    #     max_age = 34
-
-    #     model = cadre_model.Model(n=10, verbose=True)
-    #     model.run(MAXTIME=0)
-    #     mean_age_target = (18+64)/2
+        model = cadre_model.Model(n=100, verbose=True)
+        model.run(MAXTIME=0)
+        mean_age_target = (18+64)/2
 
             
-    #     for person in model.my_persons:
-    #             ages.append(person.age)
+        for person in model.my_persons:
+                ages.append(person.age)
         
-    #     print(ages)
-    #     assert(all(ages) > min_age, "all ages are not above " + str(min_age))
-    #     #assert(person.age <= max_age, "all ages are not above " + str(min_age))
-        
-       
-
-        
-
+        print("Ages are: " + str(ages))
+        self.assertTrue(all(ages) > min_age, "all ages are not above " + str(min_age))
+        self.assertTrue(all(ages) <= max_age, "all ages are not below " + str(max_age))
 
 if __name__ == '__main__':  
     unittest.main()
