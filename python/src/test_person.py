d8ee0554af799bdfb845c58b4097da575954a1ec
Aditya Khanna <khanna7.work@gmail.com>
2022-06-19 07:03:51 -0400

    test for age assignment passing

--- python/src/test_person.py
+++ python/src/test_person.py
@@ -1,26 +1,29 @@
+from statistics import mean
 import unittest
 import numpy as np
 import cadre_person
 import cadre_model 
 
-class TestSum(unittest.TestCase):
+class TestPerson(unittest.TestCase):
 
     def test_age_assignment(self):
         ages = []
         min_age = 18
-        max_age = 65
+        max_age = 64
+        mean_age_target = (min_age+max_age)/2
 
-        model = cadre_model.Model(n=100, verbose=True)
+        model = cadre_model.Model(n=10000, verbose=True)
         model.run(MAXTIME=0)
-        mean_age_target = (18+64)/2
-
-            
+                   
         for person in model.my_persons:
                 ages.append(person.age)
         
         print("Ages are: " + str(ages))
-        self.assertTrue(all(ages) > min_age, "all ages are not above " + str(min_age))
-        self.assertTrue(all(ages) <= max_age, "all ages are not below " + str(max_age))
 
+        for age in ages: 
+            self.assertTrue(age >= min_age)
+            self.assertTrue(age <= max_age)
+            self.assertEqual(round(np.mean(ages)), mean_age_target)
+        
 if __name__ == '__main__':  
     unittest.main()
