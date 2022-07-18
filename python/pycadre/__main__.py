4f439e8c7de85bf6754186268409a6df08f50c66
Aditya Khanna <khanna7.work@gmail.com>
2022-07-18 17:40:55 -0400

    hard coded parameter files

--- python/pycadre/__main__.py
+++ python/pycadre/__main__.py
@@ -9,7 +9,7 @@ def main():
     size = MPI.COMM_WORLD.Get_size()    
     rank = MPI.COMM_WORLD.Get_rank()
 
-    params_list = pycadre.load_params.load_params()
+    params_list = pycadre.load_params.load_params('../../cadre/python/myparams/model_params.yaml', '')
     STOP_AT = params_list['STOP_AT']
     N = params_list['N_AGENTS']
     model = cadre_model.Model(n=N, verbose=True, comm=MPI.COMM_WORLD)
