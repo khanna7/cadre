0524ff5c16c3f5ad25cf0452375b3684d6d426fb
Aditya Khanna <khanna7.work@gmail.com>
2022-07-22 09:43:54 -0400

    WIP: count logging, n_smokers not working

--- python/pycadre/cadre_model.py
+++ python/pycadre/cadre_model.py
@@ -9,14 +9,17 @@ from mpi4py import MPI
 import csv
 from dataclasses import dataclass, fields
 
+
 @dataclass
 class CountsLog:
-    pop_size: int=0
-    n_incarcerated: int=0
+    pop_size: int = 0
+    n_incarcerated: int = 0
+    n_current_smokers: int = 0
+
 
 class Model:
     def __init__(self, n, comm, verbose=True):
-        self.comm=comm
+        self.comm = comm
         self.my_persons = [] 
         self.graph = []
         tabular_logging_cols = ['tick', 'agent_id', 'agent_age', 'agent_race', 'agent_female', 'agent_alc_use_status', 
@@ -26,23 +29,25 @@ class Model:
 
         # initialize agents and attributes
         for i in range(n):
-            person = cadre_person.Person(name = i)  
+            person = cadre_person.Person(name=i)  
             self.my_persons.append(person)
     
         self.graph = nx.erdos_renyi_graph(len(self.my_persons), 0.001)
 
         # initialize the logging
         self.agent_logger = logging.TabularLogger(comm, load_params.params_list['agent_log_file'], tabular_logging_cols)
-        
-        agent_count = len(self.my_persons)
-        self.counts = CountsLog(agent_count)
-        loggers = logging.create_loggers(self.counts, op=MPI.SUM, rank=rank)
-        self.data_set = logging.ReducingDataSet(loggers, MPI.COMM_WORLD, load_params.params_list['counts_log_file'])
-        self.data_set.log(0)
 
+        agent_count = len(self.my_persons)
+        n_incarcerated = []
+        current_smokers = []
+
+        self.counts = CountsLog(agent_count, len(n_incarcerated), len(current_smokers))
+        loggers = logging.create_loggers(self.counts, op=MPI.SUM, rank=rank)
+        self.data_set = logging.ReducingDataSet(loggers, MPI.COMM_WORLD, 
+                        load_params.params_list['counts_log_file'])
+        #self.data_set.log(0)
 
     def log_agents(self, time):
-      
         for person in self.my_persons:
             self.agent_logger.log_row(time, person.name, round(person.age), person.race, person.female, person.alc_use_status, 
                                         person.smoker, person.last_incarceration_time, person.last_release_time, 
@@ -54,13 +59,14 @@ class Model:
         with open('counts_data.csv', 'w', newline='') as cd_file:
             
             for time in range(MAXTIME):
-                self.log_agents(time)
-                self.agent_logger.write()
-                self.data_set.log(tick=time)
-
+                
                 incaceration_states = []
                 smokers = []
                 alc_use_status = []
+
+                self.log_agents(time)
+                self.agent_logger.write()
+                self.data_set.log(tick=time)
 
                 for person in self.my_persons:
                     person.step(time) 
@@ -78,24 +84,31 @@ class Model:
                             print("Person last incarceration time: " + str(person.last_incarceration_time))
                             print("Person last release time: " + str(person.last_release_time))
                             print("Person incarceration duration: ", (person.last_release_time - person.last_incarceration_time), "\n")
-
-                    
+    
                 n = len(self.my_persons)
                 current_smokers = [i for i, x in enumerate(smokers) if x == "Current"]
                 AUD_persons = [i for i, x in enumerate(alc_use_status) if x == 3]
                 alc_abstainers = [i for i, x in enumerate(alc_use_status) if x == 0]
 
+                print("Current smokers:", current_smokers, "\n")
+                print("Number of current smokders:", len(current_smokers), "\n")
+
                 writer = csv.writer(cd_file)
                 writer.writerow([time, n, sum(incaceration_states), len(current_smokers), len(alc_abstainers), len(AUD_persons)])
 
+                self.counts.pop_size = len(self.my_persons)
+                self.counts.n_incarcerated = sum(incaceration_states)
+                self.n_current_smokers = len(current_smokers)
+                
                 self.data_set.close()
 
                 print("Number of agents is: ", len(self.my_persons))
                 print("Network size is", len(list(self.graph.nodes())), "nodes")
-                print("Network edgecound is", len(list(self.graph.edges())), "edges")
+                print("Network edgecount is", len(list(self.graph.edges())), "edges")
 
                 for line in nx.generate_edgelist(self.graph):
-                    print(line)
+                    #print(line)
+                    pass
 
 
 
