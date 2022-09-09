ce6bf1d1129b7bb1bf274ad79eefbaf98614333b
Aditya Khanna <khanna7.work@gmail.com>
2022-09-09 10:28:39 -0400

    read edge probability from YAML

--- python/pycadre/generate_network.py
+++ python/pycadre/generate_network.py
@@ -9,6 +9,7 @@ with open("myparams/model_params.yaml", 'r') as stream:
 
 n_agents = params['N_AGENTS']
 fpath = params['network_file']
-network_init = nx.erdos_renyi_graph(n_agents, 0.001)
+edge_prob = params['EDGE_PROB']
+network_init = nx.erdos_renyi_graph(n_agents, edge_prob)
 print(network_init)
 write_network(graph=network_init, network_name='network_init', fpath=fpath, n_ranks=1)
