e601cac91bfbbc97a4bf09bee9ed380d34bf377a
Aditya Khanna <aditya_khanna@brown.edu>
2022-11-18 10:08:23 -0500

    modify oscar with nick

--- swift_proj/envs/oscar.sh
+++ swift_proj/envs/oscar.sh
@@ -1,8 +1,10 @@
+module load pcre2/10.35 gcc/8.3
 module load python/3.9.0
-module load gcc/8.3
 module load mpi/openmpi_4.0.4_gcc 
-module unload java #WHY IS THIS NECESSARY?
+module unload java 
 module load R/4.0.3
+
+source /users/akhann16/code/cadre/python/new_cadre_env/bin/activate
 
 #export MPICH_ROOT=/software/mpich-3.3-el7-x86_64+gcc-9.2.0 #SINCE WE ARE LOADING OPENMPI, DO WE ALSO NEED THIS?
 export TCL_ROOT=/gpfs/home/akhann16/sfw/tcl-8.6.12
