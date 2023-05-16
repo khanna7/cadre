248df4035c98e69093c58be3b63434254493fb33
Aditya Khanna <aditya_khanna@brown.edu>
2023-05-16 09:16:41 -0400

    make consistent with daniel's commit 1901aba to set tolerances for tests to pass reliably

--- python/mytests/test_network.py
+++ python/mytests/test_network.py
@@ -28,7 +28,7 @@ class TestNetwork(unittest.TestCase):
         print("Generated mean degree is ..", mean_degree, "\n")
 
         # Test if the mean degree is approximately equal to the target_mean_degree
-        self.assertAlmostEqual(mean_degree, target_mean_degree, delta=0.1)
+        self.assertAlmostEqual(mean_degree, target_mean_degree, delta=0.2)
         
     def test_create_new_edges(self):
         network = cadre_network.ErdosReyniNetwork(MPI.COMM_WORLD, 
@@ -36,10 +36,16 @@ class TestNetwork(unittest.TestCase):
                                                   self.params_list['TARGET_MEAN_DEGREE'])
         network.init_network(self.params_list['N_AGENTS'])
         person_creator = init_person_creator()
+        N_start = network.get_num_edges()
         for i in range(self.params_list['N_AGENTS']):
             all_agents = network.get_agents()
             network.remove_agent(random.choice(all_agents))
             new_agent = person_creator.create_person()
             network.add(new_agent)
 
-        self.assertAlmostEqual(network.get_num_edges(), (self.params_list['N_AGENTS']*self.params_list['TARGET_MEAN_DEGREE'])/2, delta=10)
+        N_end = network.get_num_edges()
+        end_to_start = N_end / N_start
+        expected = self.params_list['N_AGENTS']*self.params_list['TARGET_MEAN_DEGREE']/2
+        self.assertAlmostEqual(end_to_start, 1.0, delta=0.1)
+        end_to_expected = N_end / expected
+        self.assertAlmostEqual(end_to_expected, 1.0, delta=0.1)
