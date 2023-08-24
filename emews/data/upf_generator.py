9cfbd70ef04519ead1a6e58f3a4a36d12c66b440
Aditya Khanna <aditya_khanna@brown.edu>
2023-08-24 16:56:14 -0400

    bring in from the comp-experiments branch

--- emews/data/upf_generator.py
+++ emews/data/upf_generator.py
@@ -1,35 +1,34 @@
-import yaml
-import json
 import os
+import yaml
+import itertools
+import json  
 
 # Read the YAML data
 yaml_path = os.path.join(os.sep, 'oscar', 'home', 'akhann16', 'code', 'cadre', 'python', 'myparams', 'model_params.yaml')
 with open(yaml_path, 'r') as f:
     base_data = yaml.safe_load(f)
-    
-# Define the variations for UPF
-  ## stop_at_values = [100, 200, 300]
-  ## n_agents_values = [500, 1000]
 
-# Define variations for FIRST_DEGREE onset in smoking and alcohol
+# Extract the base values 
+random_seed_base_val = base_data['random.seed']
 original_smoking_onset = base_data['SMOKING_NETWORK_EFFECTS']['ONSET']['FIRST_DEGREE']
 original_alcohol_onset = base_data['ALCOHOL_NETWORK_EFFECTS']['ONSET']['FIRST_DEGREE']
 
-smoking_onset_values = [original_smoking_onset - 0.4, original_smoking_onset - 0.2, original_smoking_onset, original_smoking_onset + 0.2, original_smoking_onset + 0.4]
-alcohol_onset_values = [original_alcohol_onset - 0.4, original_alcohol_onset - 0.2, original_alcohol_onset, original_alcohol_onset + 0.2, original_alcohol_onset + 0.4]
+# Define the range for each parameter
+random_seed_values = range(random_seed_base_val, random_seed_base_val+5)
+smoking_onset_values = [original_smoking_onset + i*0.2 for i in range(-2, 3)] # Gives [-0.4, -0.2, 0, 0.2, 0.4] offsets
+alcohol_onset_values = [original_alcohol_onset + i*0.2 for i in range(-2, 3)]
 
-# Generate UPF file
+# Generate combinations
+combinations = list(itertools.product(random_seed_values, smoking_onset_values, alcohol_onset_values))
+
+# Write to UPF
 with open('upf_output.txt', 'w') as upf:
-    for smoking_onset in smoking_onset_values:
-        for alcohol_onset in alcohol_onset_values:
-            upf_entry = {
-                'SMOKING_NETWORK_EFFECTS': {
-                    'ONSET': {'FIRST_DEGREE': smoking_onset}
-                },
-                'ALCOHOL_NETWORK_EFFECTS': {
-                    'ONSET': {'FIRST_DEGREE': alcohol_onset}
-                }
-            }
-            upf.write(json.dumps(upf_entry) + '\n')
+    for combo in combinations:
+        entry = {
+            'random.seed': combo[0],
+            'SMOKING_NETWORK_EFFECTS': {'ONSET': {'FIRST_DEGREE': combo[1]}},
+            'ALCOHOL_NETWORK_EFFECTS': {'ONSET': {'FIRST_DEGREE': combo[2]}}
+        }
+        upf.write(json.dumps(entry) + '\n')  # Convert dictionary to string before writing
 
 print("UPF generated.")
