5df57649da6f8390a2bc20a95a328c90559277ce
Aditya Khanna <aditya_khanna@brown.edu>
2023-11-05 23:56:48 -0500

    update data analysis

--- data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/utils/post-release-alcohol.R
+++ data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/utils/post-release-alcohol.R
@@ -5,23 +5,23 @@ calculate_proportions <- function(data) {
                    Proportion = as.numeric(proportions))
   df$Category <- factor(df$Category,
                         levels = c(0, 1, 2, 3),
-                        labels = c("Lifetime Abstention", "Low-Risk Use", "Heavy Use", "AUD"))
+                        labels = c("Non-Drinking", "Cat I", "Cat II", "Cat III"))
   return(df)
 }
 
 # Function to filter agents and calculate proportions for the given time period after release
-summary_after_release <- function(time_period) {
+summary_after_release <- function(time_period, agent_dt) {
   agents_within_time_period <- agent_dt[last_release_tick > 0 & tick <= last_release_tick + time_period]
   proportions <- calculate_proportions(agents_within_time_period)
   return(proportions)
 }
 
-# Time periods in ticks (days): 1 day, 1 week, 2 weeks, 1 month, 3 months, 6 months, 1 year
-time_periods <- c(1, 7, 14, 30, 90, 180, 365)
-
-# Calculate summaries for each time period
-summaries <- lapply(time_periods, function(x) summary_after_release(x))
-
-# Display summaries
-names(summaries) <- paste(time_periods, "days after release")
-summaries
+# # Time periods in ticks (days): 1 day, 1 week, 2 weeks, 1 month, 3 months, 6 months, 1 year
+# time_periods <- c(1, 7, 14, 30, 90, 180, 365)
+# 
+# # Calculate summaries for each time period
+# summaries <- lapply(time_periods, function(x) summary_after_release(x))
+# 
+# # Display summaries
+# names(summaries) <- paste(time_periods, "days after release")
+# summaries
