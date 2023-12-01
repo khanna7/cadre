4be0c629ec3cd512b516eb2084bb1b570b75786e
Aditya Khanna <aditya_khanna@brown.edu>
2023-12-01 18:13:40 -0500

    expand the here commands to compile as html from .R

--- data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/post-release-smoking-analysis.R
+++ data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/post-release-smoking-analysis.R
@@ -15,11 +15,14 @@ library(ggplot2)
 
 
 # Utility functions ------------
-source(here("agent-analysis", "utils", "post-release-smoking.R"))
+#source(here("agent-analysis", "utils", "post-release-smoking.R"))
+source("/users/akhann16/code/cadre/data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/utils/post-release-smoking.R")
+
 
 # Read RDS file ------------
 
-agent_log_env <- readRDS(here("agent-analysis", "rds-outs", "agent_log_env.RDS"))
+#agent_log_env <- readRDS(here("agent-analysis", "rds-outs", "agent_log_env.RDS"))
+agent_log_env <- readRDS("/users/akhann16/code/cadre/data-analysis-plotting/Simulated-Data-Analysis/r/agent-analysis/rds-outs/agent_log_env.RDS")
 
 
 # Load data ------------
