e42baa5f29fb59a59f0a8817a8703157ccc21d6b
Aditya Khanna <aditya_khanna@brown.edu>
2024-02-02 16:48:01 -0500

    settings file for rhel

---
+++ python/settings-rhel9-2.sh
@@ -0,0 +1,9 @@
+module load openmpi/4.1.2-s5wtoqb  
+gcc -v #check that gcc/11 is loaded
+
+module load r/4.3.1-lmofgb4
+
+
+module load  python/3.11.0s-ixrhc3q
+
+export cadre_dt_output=$(realpath /gpfs/data/akhann16/akhann16/cadre_simulated_data/)
