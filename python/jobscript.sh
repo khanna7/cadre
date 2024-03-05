4343f4678f494ece809029f63dd1391fedf41cb1
Aditya Khanna <aditya_khanna@brown.edu>
2024-03-05 14:11:51 -0500

    run model on RHEL9.2 with nick's venv

--- python/jobscript.sh
+++ python/jobscript.sh
@@ -4,13 +4,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH -n 1
 #SBATCH --mem=16G
-#SBATCH -t 10:30:00
+#SBATCH -t 00:10:00
 #SBATCH -o slurm_output/job_%j.out
 #SBATCH -e slurm_output/job_%j.err
 
-source settings.sh
+source settings-rhel9-2.sh
 module list
-ldd new_cadre_env_4/lib/python3.9/site-packages/mpi4py/MPI.cpython-39-x86_64-linux-gnu.so 
-source new_cadre_env_4/bin/activate
-python -m pycadre myparams/model_params.yaml
+
+python3 -m pycadre myparams/model_params.yaml
     
